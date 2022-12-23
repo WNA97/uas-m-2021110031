7ac3fd07a69be9d7967bbc3a05ee6af8464a7633
WNA97 <wiweka.n.a@gmail.com>
2022-12-23 23:07:19 +0700

    Terima kasih Ko Andre dan Ko Ferdinan

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -14,6 +14,7 @@ class AccountController extends Controller
      */
     public function index()
     {
+        //
         $accounts = Account::all();
         return view('accounts.index', compact('accounts'));
     }
